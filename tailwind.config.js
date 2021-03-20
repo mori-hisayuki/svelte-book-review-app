d5aca9ad1b1463b136e1b48390b0e952247130e1
hisayuki_mori <h.mori0206@gmail.com>
2021-03-21 00:13:42 +0900

    Tailwindの設定追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: !process.env.ROLLUP_WATCH,
+    mode: 'all',
+    content: ['./**/**/*.html', './**/**/*.svelte'],
+
+    options: {
+      whitelistPatterns: [/svelte-/],
+      defaultExtractor: (content) =>
+        [...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
