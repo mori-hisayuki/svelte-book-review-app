d4728b3d4a3c5c2fa1b7880db9e51417304cc41a
hisayuki_mori <h.mori0206@gmail.com>
2021-03-21 23:21:24 +0900

    SearchBookの作成

--- src/pages/SearchBook.svelte
+++ src/pages/SearchBook.svelte
@@ -1,22 +1,42 @@
 <script lang='ts'>
     import SearchBar from '../components/SearchBar.svelte'
     import RepositoryFactory from '../repositories/repositoryFactory'
+    import type { Book } from '../repositories/book/types'
 
     const bookRepository = RepositoryFactory['book']
     let query = ''
+    let books: Book[] = []
+    let promise: Promise<void>
+
 
     const handleSubmit = async () => {
-        if(!)
-        console.log('handleSubmit')
-        console.log(query)
-        const response = await bookRepository.query({ query })
-        console.log(response)
+        if(!query.trim()) return
+        promise = getBook()
     }
+
+    const getBook = async () => {
+        books = (await bookRepository.query({ query })).items
+    }
+
 </script>
 <form on:submit|preventDefault={handleSubmit}>
     <SearchBar bind:value={query} />
 </form>
 
 <div class='text-center mt-4'>
-    { query }
+    {#if !books}
+        <div>検索結果が見つかりませんでした</div>
+    {:else}
+        {#each books as book (book.id)}
+            <div>{book.volumeInfo.title}</div>
+        {/each}
+    {/if}
+    {#await promise}
+        <div>loading...</div>
+        {:catch e}
+        <span class='text-red-600 text-sm'>
+            {e.message}
+        </span>
+    {/await}
+
 </div>
