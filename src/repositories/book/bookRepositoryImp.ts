0b0126425b6686e52ff03e213d15517c93a37407
hisayuki_mori <h.mori0206@gmail.com>
2021-03-21 21:59:58 +0900

    bookRepositoryの修正

--- src/repositories/book/bookRepositoryImp.ts
+++ src/repositories/book/bookRepositoryImp.ts
@@ -5,7 +5,7 @@ import { Book, QueryParams, Response } from './types'
 
 export class BookRepositoryImp implements BookRepository {
 
-    query = async (params: QueryParams): Promise<Response> => (await httpClient.get<Response>('/', { params })).data
+    query = async (params: QueryParams): Promise<Response> => (await httpClient.get<Response>('/', { params: { q: params.query, startIndex: params.startIndex } })).data
 
     findById = async (id: string): Promise<Book> => (await httpClient.get<Book>(`/${id}`)).data
 }
