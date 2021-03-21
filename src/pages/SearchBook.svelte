4f125a2819960b85e1f992aef39ce3001479f7a3
hisayuki_mori <h.mori0206@gmail.com>
2021-03-21 21:59:43 +0900

    SearchBarの追加

--- src/pages/SearchBook.svelte
+++ src/pages/SearchBook.svelte
@@ -1,3 +1,22 @@
-<div>
-    本を探す
+<script lang='ts'>
+    import SearchBar from '../components/SearchBar.svelte'
+    import RepositoryFactory from '../repositories/repositoryFactory'
+
+    const bookRepository = RepositoryFactory['book']
+    let query = ''
+
+    const handleSubmit = async () => {
+        if(!)
+        console.log('handleSubmit')
+        console.log(query)
+        const response = await bookRepository.query({ query })
+        console.log(response)
+    }
+</script>
+<form on:submit|preventDefault={handleSubmit}>
+    <SearchBar bind:value={query} />
+</form>
+
+<div class='text-center mt-4'>
+    { query }
 </div>
