cfa696ca596a7430d544bb9cc951a9179f770aaf
hisayuki_mori <h.mori0206@gmail.com>
2021-03-21 19:38:41 +0900

    add repositoryFactory

---
+++ src/repositories/repositoryFactory.ts
@@ -0,0 +1,9 @@
+import { BookRepository, BookRepositoryImp } from './book'
+
+export type Repositories {
+    ['book']: BookRepository
+}
+
+export default {
+    ['book']: new BookRepositoryImp()
+} as Repositories
